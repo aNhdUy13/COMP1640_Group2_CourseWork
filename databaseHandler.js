2e8299249b25b61fda041ddede59d4a4b4c33dfe
Duy Anh Nguyen <73863929+aNhdUy13@users.noreply.github.com>
2022-02-24 11:46:49 +0700

    updatea db handler

---
+++ databaseHandler.js
@@ -0,0 +1,9 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://nguyenduyanh131201:duyanh12345678@cluster0.3vt1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbName = "COMP1640_Project";
+
+async function getDBO() {
+    const client = await MongoClient.connect(url);
+    const dbo = client.db(dbName);
+    return dbo;
+}

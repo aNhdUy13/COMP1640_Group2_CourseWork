2f2123d3a0afe83df387d26c82afddef8f9ca5fc
Duy Anh Nguyen <73863929+aNhdUy13@users.noreply.github.com>
2022-02-26 16:55:51 +0700

    update main page

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ var bodyParser = require("body-parser");
 //const { Console } = require('console');
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const viewPath = path.join(__dirname, 'views/partial')
+hbs.registerPartials(viewPath)
+
+
 app.get('/', (req, res) => {
     // res.send("HEllo World");
     res.render('index');
 })
+
+
+
+
+
+
+app.use(express.static('public'));
+
 
 const PORT = 12345;
 app.listen(process.env.PORT || PORT);

8827cc68de33fe5d3ebab92702cfef45949b279d
Duy Anh Nguyen <73863929+aNhdUy13@users.noreply.github.com>
2022-03-19 17:15:21 +0700

    update new database v2

--- databaseHandler.js
+++ databaseHandler.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://nguyenduyanh131201:duyanh12345678@cluster0.3vt1h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const dbName = "COMP1640_Project";
+var url = "mongodb+srv://nguyenduyanh131201:duyanh12345678@cluster0.letwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbName = "COMP1640_Web_DBnew_2";
 
 // Import dependencies to hash passwordToCompare
 const bcrypt = require('bcrypt');
